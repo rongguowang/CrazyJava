9cf0b8f84a3657ec7297827fab097c02e8b33c5e
rongguowang <godenwang@gmail.com>
2012-04-21 17:09:25 +0800

    CrazyJava: add the BasicTest java source file
    the BasicTest java source file mainly do some basic test of the
    pure java programing language. treat it as nothing.

---
+++ src/org/crazyjava/sample/BasicTest.java
@@ -0,0 +1,15 @@
+package org.crazyjava.sample;
+
+public class BasicTest {
+    public int i;
+    public char a;
+    
+    public static void main(String[] args) {
+    	BasicTest bt = new BasicTest();
+    	System.out.println("i = " + bt.i);
+    	System.out.println();
+    	System.out.println("a = " + bt.a);
+
+    }
+	
+}
